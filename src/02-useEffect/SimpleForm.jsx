d631d49061c1dc85d9d99498300c21d1fdf55ba0
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-11 23:15:22 -0600

    practicing with return in useEffect and why is used when a component is unmounted

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Message } from './Message';
 
 export const SimpleForm = () => {
   const [formState, setFormState] = useState({
@@ -18,15 +19,15 @@ export const SimpleForm = () => {
   };
 
   useEffect(() => {
-    console.log('useEffect was called');
+    // console.log('useEffect was called');
   }, []);
 
   useEffect(() => {
-    console.log('useEffect was called by form change');
+    // console.log('useEffect was called by form change');
   }, [formState]);
 
   useEffect(() => {
-    console.log('useEffect was called by email change');
+    // console.log('useEffect was called by email change');
   }, [email]);
 
   return (
@@ -51,6 +52,8 @@ export const SimpleForm = () => {
         value={email}
         onChange={onInputChange}
       />
+
+      {username === 'user' && <Message />}
     </>
   );
 };
