71c9a3464710137656abea0d68be27c0d53439cb
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-11 23:39:58 -0600

    creating custom hook for Form state management

---
+++ src/hooks/useForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export const useForm = (initialForm = {}) => {
+  const [formState, setFormState] = useState(initialForm);
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+
+    // use computed properties to make this flexible, and avoid defining one for each one
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // here spread to return formState properties, useful when unknow or a lot
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+  };
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterCustomHook } from './01-useState/CounterCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <SimpleForm />
+  <FormWithCustomHook />
   // </React.StrictMode>
 );
