188189a98a2652c9e6da6b047f2162f19755d73a
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-12 12:33:05 -0600

    first advances on component and trick to prevent errors when destructuring

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,13 +4,25 @@ export const MultipleCustomHooks = () => {
   const { data, isLoading, hasError } = useFetch(
     'https://rickandmortyapi.com/api/character/1'
   );
-
-  console.log({ data, isLoading, hasError });
+  // if data has values, destructure name and species
+  // useful to  avoids errors when working with potentially null or undefined values
+  const { name, species } = !!data && data;
 
   return (
     <>
       <h1>Rick n Morty API</h1>
       <hr />
+
+      {isLoading ? (
+        <div className='alert alert-info text-center'>Loading...</div>
+      ) : (
+        <blockquote className='blockquote text-end'>
+          <p className='mb-1'>{name}</p>
+          <footer className='blockquote-footer'>{species}</footer>
+        </blockquote>
+      )}
+
+      <button className='btn btn-primary'>Next quote</button>
     </>
   );
 };
