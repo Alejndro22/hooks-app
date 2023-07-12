3a90fd98926fcd432911d277f04a4f31bd8d6f08
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-11 19:29:40 -0600

    custom hook for counter and exposing hook methods

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HooksApp } from './HooksApp';
+// import { CounterApp } from './01-useState/CounterApp';
+import { CounterCustomHook } from './01-useState/CounterCustomHook';
 import './index.css';
-import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterCustomHook />
   </React.StrictMode>
 );
