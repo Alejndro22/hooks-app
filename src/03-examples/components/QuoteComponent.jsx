ea0418801c091ea605b7b86b6a00a52ec5caefda
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-14 12:09:46 -0600

    Understanding useLayoutEffect hook
    run synchronously after all DOM mutations are applied and before the browser paints the screen, useful to access layout information after a component has been rendered

--- src/03-examples/components/QuoteComponent.jsx
+++ src/03-examples/components/QuoteComponent.jsx
@@ -1,11 +1,30 @@
 import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const QuoteComponent = ({ name, species }) => {
+  const pRef = useRef();
+
+  const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({ height, width });
+  }, [name]);
+
   return (
-    <blockquote className='blockquote text-center'>
-      <p className='mb-2'>{name}</p>
-      <footer className='blockquote-footer'>{species}</footer>
-    </blockquote>
+    <>
+      <blockquote
+        className='blockquote text-center'
+        style={{ display: 'flex' }}
+      >
+        <p ref={pRef} className='mb-2'>
+          {name}
+        </p>
+        <footer className='blockquote-footer'>{species}</footer>
+      </blockquote>
+
+      <code>{JSON.stringify(boxSize)}</code>
+    </>
   );
 };
 
