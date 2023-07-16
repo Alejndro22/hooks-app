cef210548ea7abee2f93ba9c61c8c4f805dc117c
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-16 00:42:42 -0600

    Initial design for further excercises

--- src/08-useReducer/ToDoApp.jsx
+++ src/08-useReducer/ToDoApp.jsx
@@ -19,14 +19,42 @@ export const ToDoApp = () => {
 
   return (
     <>
-      <h1>To Do App</h1>
+      <h1>
+        To Do App: 10, <small>Pending: 2</small>
+      </h1>
       <hr />
 
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+      <div className='row'>
+        <div className='col-7'>
+          <ul className='list-group'>
+            {toDos.map((toDo) => (
+              <li
+                key={toDo.id}
+                className='list-group-item d-flex justify-content-between'
+              >
+                <span className='align-self-center'>Item 1</span>
+                <button className='btn btn-danger'>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='col-5'>
+          <h4>Add toDo</h4>
+          <hr />
+          <form>
+            <input
+              type='text'
+              placeholder='Next task?'
+              className='form-control'
+            />
+
+            <button type='submit' className='btn btn-outline-primary mt-4'>
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
