52f3ab6d7eb0bf826c349c239bc104bb6a35d855
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-15 19:14:39 -0600

    First steps on understanding useCallback hook
    Used to memoize functions between re-renders, this only updates if one of it's dependencies changes

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Here I use React.memo again, because is useful to memoize this comonent
+// to prevent re-rendering if props don't change, and since the same function
+// is called (because on the parent useCallback is used) it points to the same
+// memory location
+
+// eslint-disable-next-line react/display-name
+export const ShowIncrement = React.memo(({ increment }) => {
+  console.log('i regenerated :(');
+
+  return (
+    <button
+      className='btn btn-primary'
+      onClick={() => {
+        increment();
+      }}
+    >
+      Increment
+    </button>
+  );
+});
