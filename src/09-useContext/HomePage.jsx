342b754a9e9166aa845dfbc0d72db76dd28842d7
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-07-21 19:42:22 -0600

    tests with context on <HomePage /> and <LoginPage />
    when user is null and when is not, and to make sure that setUser is called when button is clicked

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -11,7 +11,7 @@ export const HomePage = () => {
       </h1>
       <hr />
 
-      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <pre aria-label='test-span'>{JSON.stringify(user, null, 3)}</pre>
     </>
   );
 };
